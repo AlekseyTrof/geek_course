4b2f57c8e55779ec060952b4b883cf5bbbc8ae7d
AlekseyTrof <alekcey.trofimov.99@gmail.com>
2025-01-12 14:06:14 +0300

    added lesson1.1

---
+++ src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
@@ -0,0 +1,45 @@
+package ru.geekbrains.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSing(3, 5);
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSing(int a, int b) {
+        if ((a + b) >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 0;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 0;
+        int b = 1;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
